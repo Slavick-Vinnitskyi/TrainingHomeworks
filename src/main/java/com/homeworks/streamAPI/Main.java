17f95ddd2309d3fec9f2528901bbe1b5b5ed36f6
Viacheslav_Vinnytsky <andrey.belyi1@gmail.com>
2019-02-05 15:14:48 +0200

    improvements in stream readability

--- src/main/java/com/homeworks/streamAPI/Main.java
+++ src/main/java/com/homeworks/streamAPI/Main.java
@@ -3,34 +3,49 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new Random().ints(10, 1, 10).toArray();
+        int[] array = new Random()
+                .ints(10, 1, 10)
+                .toArray();
         System.out.print("array : ");
-        Arrays.stream(array).forEach(x -> System.out.print(x + " "));
+        Arrays.stream(array)
+                .forEach(x -> System.out.print(x + " "));
 
-        double mediumValue = Arrays.stream(array).average().getAsDouble();
+        double mediumValue = Arrays.stream(array)
+                .average()
+                .getAsDouble();
         System.out.printf("\navarage : %.2f\n", mediumValue);
 
-        int minValue = Arrays.stream(array).min().getAsInt();
+        int minValue = Arrays.stream(array)
+                .min()
+                .getAsInt();
         System.out.printf("min value : %d\n", minValue);
 
-        int[] minValueIndex = IntStream.range(0, array.length).filter(i -> array[i] == minValue).toArray();
+        int[] minValueIndex = IntStream.range(0, array.length)
+                .filter(i -> array[i] == minValue)
+                .toArray();
         System.out.print("index : ");
         Arrays.stream(minValueIndex).forEach(x -> System.out.print(x + " "));
 
-        long countZeroElement = Arrays.stream(array).filter(x -> x == 0).count();
+        long countZeroElement = Arrays.stream(array)
+                .filter(x -> x == 0)
+                .count();
         System.out.printf("count of zero element : %d \n", countZeroElement);
 
-        long countBiggerZeroElement = Arrays.stream(array).filter(x -> x > 0).count();
+        long countBiggerZeroElement = Arrays.stream(array)
+                .filter(x -> x > 0)
+                .count();
         System.out.printf("count of bigger then zero element : %d \n", countBiggerZeroElement);
 
         int multiplicationNumber = 4;
-        int[] multipleArray = Arrays.stream(array).map((x) -> x *= multiplicationNumber).toArray();
+        int[] multipleArray = Arrays.stream(array)
+                .map((x) -> x *= multiplicationNumber)
+                .toArray();
         System.out.printf("array multiplicated by %d: ", multiplicationNumber);
         Arrays.stream(multipleArray).forEach(x -> System.out.print(x + " "));
 
-
         List<Integer> list = new ArrayList<>(Arrays.asList(4, 3, 2, 5, 3, 4));
         list.removeIf(x -> x == 4);
+        System.out.println();
         list.forEach(System.out::print);
 
         HashMap<Integer, String> map = new HashMap<>();
