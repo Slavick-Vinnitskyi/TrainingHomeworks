20ac25b058e6a08756b0efbe65fd0c305246f978
Viacheslav_Vinnytsky <andrey.belyi1@gmail.com>
2019-02-07 12:30:06 +0200

    add quickSort to algorithms

---
+++ src/main/java/com/homeworks/algorithms/sortings/QuickSort.java
@@ -0,0 +1,39 @@
+package com.homeworks.algorithms.sortings;
+
+public class QuickSort implements SortAlgorithm{
+    @Override
+    public int[] sort(int[] inputArray) {
+
+        return quickSort(inputArray, 0 , inputArray.length);
+    }
+
+    public int[] quickSort(int[] inputArray, int left, int right) {
+        if(right - left <= 1)
+            return inputArray;
+        int lleft = left;
+        int rright = right -1;
+        int pivot = inputArray[(left + (right- left) / 2)];
+        while (lleft <= rright) {
+            while (inputArray[lleft] < pivot) {
+                lleft++;
+            }
+            while (inputArray[rright] > pivot) {
+                rright--;
+            }
+            if(lleft <= rright) {
+                swap(inputArray, lleft, rright);
+                lleft++;
+                rright--;
+            }
+        }
+        if(left < rright) this.quickSort(inputArray, left, rright);
+        if(lleft < right) this.quickSort(inputArray, lleft, right);
+        return inputArray;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
