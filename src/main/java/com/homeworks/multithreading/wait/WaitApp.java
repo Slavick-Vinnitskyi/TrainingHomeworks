1fb1aad24277322ec3c2454749bd6673f30aa5f7
Славик Винницкий <andrey.belyi1@gmail.com>
2019-02-10 19:26:26 +0200

    add wait() and notify() example to multithreading package

---
+++ src/main/java/com/homeworks/multithreading/wait/WaitApp.java
@@ -0,0 +1,27 @@
+package com.homeworks.multithreading.wait;
+
+public class WaitApp {
+    public static void main(String[] args) throws InterruptedException {
+        Object lock = new Object();
+        // task будет ждать, пока его не оповестят через lock
+        Runnable task = () -> {
+            synchronized (lock) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    System.out.println("interrupted");
+                }
+            }
+            // После оповещения нас мы будем ждать, пока сможем взять лок
+            System.out.println("thread");
+        };
+        Thread taskThread = new Thread(task);
+        taskThread.start();
+        // Ждём и после этого забираем себе лок, оповещаем и отдаём лок
+        Thread.sleep(3000);
+        System.out.println("main");
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+}
